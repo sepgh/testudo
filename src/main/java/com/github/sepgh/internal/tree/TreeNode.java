17a54479bd14bfdec1b6191ab0abe56dc03f0539
sepgh <sepehr.null+gpg@gmail.com>
2024-04-23 15:55:27 +0330

    feat(b+tree): iterators of TreeNode for keys, key value pair, children
    Signed-off-by: sepgh <sepehr.null+gpg@gmail.com>

--- src/main/java/com/github/sepgh/internal/tree/TreeNode.java
+++ src/main/java/com/github/sepgh/internal/tree/TreeNode.java
@@ -1,35 +1,114 @@
 package com.github.sepgh.internal.tree;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Optional;
 
 // Todo: This isn't reactive, but should it be?
 // Todo: If AsyncFileChannels are used, do we need to use Futures here? (probably not?)
 
-@AllArgsConstructor
+/*
+  Structure of a node in binary for non-leaf
+  [1 byte IS_LEAF] + [POINTER_SIZE bytes child] + (([LONG_SIZE bytes id] + [POINTER_SIZE bytes child]) * max node size)
+
+  Structure of a node in binary for leaf
+  [1 byte IS_LEAF] + ([LONG_SIZE bytes id] + [POINTER_SIZE bytes data]) * max node size) + [POINTER_SIZE bytes previous leaf node] + [POINTER_SIZE bytes next leaf node]
+*/
 public class TreeNode {
+    public static byte IS_LEAF_NODE = 0x01;
+
+    @Setter
+    @Getter
+    private Pointer nodePointer;
     @Getter
     private final byte[] data;
-    @Getter
-    private final int nodeSize;
 
-    public Optional<TreeNode> getChildAtIndex(int index){
-        return Optional.empty();  // Todo
+    public TreeNode(byte[] data) {
+        this.data = data;
     }
 
-    public Iterator<TreeNode> children(){
+    public boolean isLeaf(){
+        return getData()[0] == IS_LEAF_NODE;
+    }
+
+    public Optional<Pointer> getChildAtIndex(int index){
+        if (this.isLeaf())
+            return Optional.empty();
+
+        return Optional.of(TreeNodeUtils.getPointerAtIndex(this, index));
+    }
+
+    public Iterator<Pointer> children(){
         return new TreeNodeChildrenIterator(this);
     }
 
-    public TreeNode getChild(int index){
-        // todo
-        return null;
+    public Iterator<Long> keys(){
+        return new TreeNodeKeysIterator(this);
     }
 
-    private static class TreeNodeChildrenIterator implements Iterator<TreeNode> {
+    public Optional<Iterator<Map.Entry<Long, Pointer>>> keyValues(){
+        if (!this.isLeaf()){
+            return Optional.empty();
+        }
+        return Optional.of(new TreeNodeKeyValueIterator(this));
+    }
+
+    private static class TreeNodeKeyValueIterator implements Iterator<Map.Entry<Long, Pointer>> {
+        private final TreeNode node;
+        private int cursor; // Cursor points to current key index, not current byte
+        private boolean hasNext = true;
+
+        private TreeNodeKeyValueIterator(TreeNode node) {
+            this.node = node;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (!hasNext)
+                return false;
+
+            hasNext = TreeNodeUtils.hasKeyValuePointerAtIndex(this.node, cursor);
+            return hasNext;
+        }
+
+        @Override
+        public Map.Entry<Long, Pointer> next() {
+            Map.Entry<Long, Pointer> value = TreeNodeUtils.getKeyValuePointerAtIndex(this.node, cursor);
+            cursor++;
+            return value;
+        }
+    }
+
+    private static class TreeNodeKeysIterator implements Iterator<Long> {
+        private final TreeNode node;
+        private int cursor; // Cursor points to current key index, not current byte
+        private boolean hasNext = true;
+
+        private TreeNodeKeysIterator(TreeNode node) {
+            this.node = node;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (!hasNext)
+                return false;
+
+            hasNext = TreeNodeUtils.hasKeyAtIndex(this.node, cursor);
+            return hasNext;
+        }
+
+        @Override
+        public Long next() {
+            long value = TreeNodeUtils.getKeyAtIndex(this.node, cursor);
+            cursor++;
+            return value;
+        }
+    }
+
+    private static class TreeNodeChildrenIterator implements Iterator<Pointer> {
 
         private final TreeNode node;
         private int cursor;  // Cursor points to current pointer index, not current byte
@@ -44,15 +123,15 @@ public class TreeNode {
             if (!this.hasNext)
                 return false;
 
-            hasNext = TreeNodeUtils.hasPointerAtIndex(this.node, cursor);
+            hasNext = TreeNodeUtils.hasChildPointerAtIndex(this.node, cursor);
             return hasNext;
         }
 
         @Override
-        public TreeNode next() {
+        public Pointer next() {
             Pointer pointer = TreeNodeUtils.getPointerAtIndex(this.node, this.cursor);
             this.cursor++;
-            return null;  // Todo: construct a node from pointer
+            return pointer;
         }
     }
 }
--- src/main/java/com/github/sepgh/internal/tree/TreeNodeUtils.java
+++ src/main/java/com/github/sepgh/internal/tree/TreeNodeUtils.java
@@ -1,23 +1,65 @@
 package com.github.sepgh.internal.tree;
 
+import com.github.sepgh.internal.utils.BinaryUtils;
+
+import java.util.AbstractMap;
+import java.util.Map;
+
 public class TreeNodeUtils {
+    private static final int TREE_NODE_FLAGS_END = 1;
 
     /**
      * @return bool true, if the first byte of the current cursor position is not 0x00
      * Note that index is the index of the pointer object we want to refer to, not the byte position in byte array
      */
-    public static boolean hasPointerAtIndex(TreeNode treeNode, int index){
-        if (index == 0){
-            return treeNode.getData()[0] != 0x00;
+    public static boolean hasChildPointerAtIndex(TreeNode treeNode, int index){
+        if (TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES)) + Pointer.POINTER_SIZE > treeNode.getData().length)
+            return false;
+
+        if (!treeNode.isLeaf()){
+            return treeNode.getData()[TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES))] == Pointer.TYPE_NODE;
         }
 
-        return treeNode.getData()[(index * (Pointer.POINTER_SIZE + Long.BYTES)) + Pointer.POINTER_SIZE] != 0x00;
+        return false;
     }
 
     public static Pointer getPointerAtIndex(TreeNode treeNode, int index){
-        if (index == 0)
-            return Pointer.fromByteArray(treeNode.getData());
-        return Pointer.fromByteArray(treeNode.getData(), (index * (Pointer.POINTER_SIZE + Long.BYTES)) + Pointer.POINTER_SIZE);
+        return Pointer.fromByteArray(treeNode.getData(), TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES)));
+    }
+
+    private static int getKeyStartIndex(TreeNode treeNode, int index) {
+        if (!treeNode.isLeaf()){
+            return TREE_NODE_FLAGS_END + Pointer.POINTER_SIZE + (index * (Long.BYTES + Pointer.POINTER_SIZE));
+        } else {
+            return TREE_NODE_FLAGS_END + (index * (Long.BYTES + Pointer.POINTER_SIZE));
+        }
+    }
+
+    public static boolean hasKeyAtIndex(TreeNode treeNode, int index){
+        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        if (keyStartIndex + Long.BYTES > treeNode.getData().length)
+            return false;
+
+        return BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex) != 0;
+    }
+
+    public static long getKeyAtIndex(TreeNode treeNode, int index) {
+        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        return BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex);
+    }
+
+    public static boolean hasKeyValuePointerAtIndex(TreeNode treeNode, int index){
+        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        return keyStartIndex + Long.BYTES + Pointer.POINTER_SIZE > treeNode.getData().length ||
+                treeNode.getData()[keyStartIndex + Long.BYTES + Pointer.POINTER_SIZE] != Pointer.TYPE_DATA;
+    }
+
+    public static Map.Entry<Long, Pointer> getKeyValuePointerAtIndex(TreeNode treeNode, int index) {
+        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        return new AbstractMap.SimpleImmutableEntry<>(
+            BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex),
+            Pointer.fromByteArray(treeNode.getData(), keyStartIndex + Long.BYTES)
+        );
     }
 
 }
