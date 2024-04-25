33b7a3a9f1f03d4a26b2c982956d2615692e6f8d
sepgh <sepehr.null+gpg@gmail.com>
2024-04-25 16:28:46 +0330

    refactor: cleanup TreeNodeUtils
    Signed-off-by: sepgh <sepehr.null+gpg@gmail.com>

--- src/main/java/com/github/sepgh/internal/tree/Pointer.java
+++ src/main/java/com/github/sepgh/internal/tree/Pointer.java
@@ -3,7 +3,6 @@ package com.github.sepgh.internal.tree;
 import com.github.sepgh.internal.utils.BinaryUtils;
 import com.google.common.primitives.Ints;
 import com.google.common.primitives.Longs;
-import lombok.Setter;
 
 /*
    A pointer may point to where data begins (on table) or to another node
@@ -13,7 +12,7 @@ import lombok.Setter;
 public record Pointer(byte type, long position, int chunk) {
     public static byte TYPE_DATA = 0x01;
     public static byte TYPE_NODE = 0x02;
-    public static int POINTER_SIZE = 1 + Long.BYTES + Integer.BYTES;
+    public static int BYTES = 1 + Long.BYTES + Integer.BYTES;
 
     public static Pointer fromByteArray(byte[] bytes, int position){
         return new Pointer(
@@ -28,14 +27,15 @@ public record Pointer(byte type, long position, int chunk) {
     }
 
     public byte[] toByteArray() {
-        byte[] bytes = new byte[POINTER_SIZE];
+        byte[] bytes = new byte[BYTES];
         bytes[0] = type;
         System.arraycopy(Longs.toByteArray(position), 0, bytes, 1, Long.BYTES);
         System.arraycopy(Ints.toByteArray(chunk), 0, bytes, 1 + Long.BYTES, Integer.BYTES);
         return bytes;
     }
 
-    public void fillByteArray(byte[] source, int position) {
+    // A quick way to write the current pointer into a specific position of a byte array (which is node itself)
+    public void fillByteArrayWithPointer(byte[] source, int position) {
         source[position] = type;
         System.arraycopy(Longs.toByteArray(this.position), 0, source, position + 1, Long.BYTES);
         System.arraycopy(Ints.toByteArray(chunk), 0, source, position + 1 + Long.BYTES, Integer.BYTES);
--- src/main/java/com/github/sepgh/internal/tree/TreeNodeUtils.java
+++ src/main/java/com/github/sepgh/internal/tree/TreeNodeUtils.java
@@ -8,88 +8,221 @@ import java.util.AbstractMap;
 import java.util.Map;
 
 public class TreeNodeUtils {
-    private static final int TREE_NODE_FLAGS_END = 1;
+    private static final int OFFSET_TREE_NODE_FLAGS_END = 1;
+    private static final int OFFSET_INTERNAL_NODE_KEY_BEGIN = OFFSET_TREE_NODE_FLAGS_END + Pointer.BYTES;
+    private static final int OFFSET_LEAF_NODE_KEY_BEGIN = OFFSET_TREE_NODE_FLAGS_END;
+    private static final int SIZE_INTERNAL_NODE_KEY_POINTER = Long.BYTES + Pointer.BYTES;
+    private static final int SIZE_LEAF_NODE_KEY_POINTER = Long.BYTES + Pointer.BYTES;
 
     /**
+     *
+     * @param treeNode node to read/write from/to
+     * @param index to check child pointer
      * @return bool true, if the first byte of the current cursor position is not 0x00
-     * Note that index is the index of the pointer object we want to refer to, not the byte position in byte array
+     *         Note that index is the index of the pointer object we want to refer to, not the byte position in byte array
      */
     public static boolean hasChildPointerAtIndex(AbstractTreeNode treeNode, int index){
-        if (TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES)) + Pointer.POINTER_SIZE > treeNode.getData().length)
+        if (OFFSET_TREE_NODE_FLAGS_END + (index * (Pointer.BYTES + Long.BYTES)) > treeNode.getData().length)
             return false;
 
-        if (!treeNode.isLeaf()){
-            return treeNode.getData()[TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES))] == Pointer.TYPE_NODE;
-        }
 
-        return false;
+        return treeNode.getData()[OFFSET_TREE_NODE_FLAGS_END + (index * (Pointer.BYTES + Long.BYTES))] == Pointer.TYPE_NODE;
     }
 
-    public static Pointer getPointerAtIndex(AbstractTreeNode treeNode, int index){
-        return Pointer.fromByteArray(treeNode.getData(), TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES)));
+
+    /**
+     * @param treeNode node to read/write from/to
+     * @param index to check child pointer
+     * @return Pointer to child node at index
+     */public static Pointer getChildPointerAtIndex(AbstractTreeNode treeNode, int index){
+        return Pointer.fromByteArray(treeNode.getData(), OFFSET_TREE_NODE_FLAGS_END + (index * (Pointer.BYTES + Long.BYTES)));
     }
 
+    /**
+     * @param treeNode node to read/write from/to
+     * @param index index of the pointer to set
+     * @param pointer object to set
+     */
     public static void setPointerToChild(AbstractTreeNode treeNode, int index, Pointer pointer){
         if (index == 0){
-            System.arraycopy(pointer.toByteArray(), 0, treeNode.getData(), TREE_NODE_FLAGS_END, Pointer.POINTER_SIZE);
+            System.arraycopy(pointer.toByteArray(), 0, treeNode.getData(), OFFSET_TREE_NODE_FLAGS_END, Pointer.BYTES);
         } else {
             System.arraycopy(
                     pointer.toByteArray(),
                     0,
                     treeNode.getData(),
-                    TREE_NODE_FLAGS_END + (index * (Pointer.POINTER_SIZE + Long.BYTES)),
-                    Pointer.POINTER_SIZE
+                    OFFSET_TREE_NODE_FLAGS_END + (index * (Pointer.BYTES + Long.BYTES)),
+                    Pointer.BYTES
             );
         }
     }
 
-    private static int getKeyStartIndex(AbstractTreeNode treeNode, int index) {
+    /**
+     * @param treeNode to read/write from/to
+     * @param index of the key we are looking for
+     * @return the offset where the key is found at
+     */
+    private static int getKeyStartOffset(AbstractTreeNode treeNode, int index) {
         if (!treeNode.isLeaf()){
-            return TREE_NODE_FLAGS_END + Pointer.POINTER_SIZE + (index * (Long.BYTES + Pointer.POINTER_SIZE));
+            return OFFSET_INTERNAL_NODE_KEY_BEGIN + (index * (Long.BYTES + Pointer.BYTES));
         } else {
-            return TREE_NODE_FLAGS_END + (index * (Long.BYTES + Pointer.POINTER_SIZE));
+            return OFFSET_TREE_NODE_FLAGS_END + (index * (Long.BYTES + Pointer.BYTES));
         }
     }
 
+    /**
+     * @param treeNode to read/write from/to
+     * @param index of the key to check existence
+     * @return boolean state of existence of a key in index
+     */
     public static boolean hasKeyAtIndex(AbstractTreeNode treeNode, int index){
-        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        int keyStartIndex = getKeyStartOffset(treeNode, index);
         if (keyStartIndex + Long.BYTES > treeNode.getData().length)
             return false;
 
         return BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex) != 0;
     }
 
+    /**
+     * @param treeNode to read/write from/to
+     * @param index to read they key at
+     * @return key value at index
+     */
     public static long getKeyAtIndex(AbstractTreeNode treeNode, int index) {
-        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        int keyStartIndex = getKeyStartOffset(treeNode, index);
         return BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex);
     }
 
     public static boolean hasKeyValuePointerAtIndex(AbstractTreeNode treeNode, int index){
-        int keyStartIndex = getKeyStartIndex(treeNode, index);
-        return keyStartIndex + Long.BYTES + Pointer.POINTER_SIZE <= treeNode.getData().length &&
+        int keyStartIndex = getKeyStartOffset(treeNode, index);
+        return keyStartIndex + SIZE_LEAF_NODE_KEY_POINTER <= treeNode.getData().length &&
                 treeNode.getData()[keyStartIndex + Long.BYTES] == Pointer.TYPE_DATA;
     }
 
     public static Map.Entry<Long, Pointer> getKeyValuePointerAtIndex(AbstractTreeNode treeNode, int index) {
-        int keyStartIndex = getKeyStartIndex(treeNode, index);
+        int keyStartIndex = getKeyStartOffset(treeNode, index);
         return new AbstractMap.SimpleImmutableEntry<>(
             BinaryUtils.bytesToLong(treeNode.getData(), keyStartIndex),
             Pointer.fromByteArray(treeNode.getData(), keyStartIndex + Long.BYTES)
         );
     }
 
-    public static void setKeyValueAtIndex(AbstractTreeNode treeNode, int index, long identifier, Pointer pointer) {
+    public static void setKeyValueAtIndex(AbstractTreeNode treeNode, int index, long key, Pointer pointer) {
         System.arraycopy(
-                Longs.toByteArray(identifier),
+                Longs.toByteArray(key),
                 0,
                 treeNode.getData(),
-                TREE_NODE_FLAGS_END + (index * (Long.BYTES + Pointer.POINTER_SIZE)),
+                OFFSET_LEAF_NODE_KEY_BEGIN + (index * (SIZE_LEAF_NODE_KEY_POINTER)),
                 Long.BYTES
         );
 
-        pointer.fillByteArray(
+        pointer.fillByteArrayWithPointer(
                 treeNode.getData(),
-                TREE_NODE_FLAGS_END + (index * (Long.BYTES + Pointer.POINTER_SIZE)) + Long.BYTES
+                OFFSET_LEAF_NODE_KEY_BEGIN + (index * (SIZE_LEAF_NODE_KEY_POINTER)) + Long.BYTES
         );
+    }
+
+    public static int setKeyValue(AbstractTreeNode treeNode, long key, Pointer pointer) {
+
+        int indexToFill = 0;
+        Map.Entry<Long, Pointer> keyValueAtIndex = null;
+        for (int i = 0; i < ((treeNode.getData().length - 1) / (Long.BYTES + Pointer.BYTES)); i++){
+            keyValueAtIndex = getKeyValuePointerAtIndex(treeNode, i);
+
+            if (keyValueAtIndex.getKey() == 0 || key < keyValueAtIndex.getKey()){
+                indexToFill = i;
+                break;
+            }
+
+        }
+
+        if (keyValueAtIndex == null || keyValueAtIndex.getKey() == 0){
+            setKeyValueAtIndex(treeNode, indexToFill, key, pointer);
+        } else {
+            byte[] temp = new byte[treeNode.getData().length - (OFFSET_LEAF_NODE_KEY_BEGIN + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)))];
+            System.arraycopy(
+                    treeNode.getData(),
+                    OFFSET_LEAF_NODE_KEY_BEGIN + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)),
+                    temp,
+                    0,
+                    temp.length
+            );
+
+            System.arraycopy(
+                    Longs.toByteArray(key),
+                    0,
+                    treeNode.getData(),
+                    OFFSET_TREE_NODE_FLAGS_END + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)),
+                    Long.BYTES
+            );
+
+            System.arraycopy(
+                    temp,
+                    0,
+                    treeNode.getData(),
+                    OFFSET_TREE_NODE_FLAGS_END + ((indexToFill + 1) * (SIZE_LEAF_NODE_KEY_POINTER)),
+                    Long.BYTES
+            );
+        }
+
+        return indexToFill;
+
+    }
+
+    public static int addKeyAndGetIndex(AbstractTreeNode treeNode, long key) {
+        // Shall only be called on internal nodes
+
+        int indexToFill = 0;
+        long keyAtIndex = 0;
+        for (int i = 0; i < ((treeNode.getData().length - 1) / (Long.BYTES + Pointer.BYTES)); i++){
+            keyAtIndex = getKeyAtIndex(treeNode, i);
+
+            if (keyAtIndex == 0 || key < keyAtIndex){
+                indexToFill = i;
+                break;
+            }
+
+        }
+
+        if (keyAtIndex == 0){
+            System.arraycopy(
+                    Longs.toByteArray(key),
+                    0,
+                    treeNode.getData(),
+                    OFFSET_INTERNAL_NODE_KEY_BEGIN + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)),
+                    Long.BYTES
+            );
+        } else {
+            // Copy existing bytes to a temporary location
+            byte[] temp = new byte[treeNode.getData().length - (OFFSET_TREE_NODE_FLAGS_END + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)))];
+            System.arraycopy(
+                    treeNode.getData(),
+                    OFFSET_INTERNAL_NODE_KEY_BEGIN + (indexToFill * (SIZE_LEAF_NODE_KEY_POINTER)),
+                    temp,
+                    0,
+                    temp.length
+            );
+
+            // Write
+            System.arraycopy(
+                    Longs.toByteArray(key),
+                    0,
+                    treeNode.getData(),
+                    OFFSET_INTERNAL_NODE_KEY_BEGIN + (indexToFill * (Long.BYTES + Pointer.BYTES)),
+                    Long.BYTES
+            );
+
+            // Copy temp bytes back to valid position
+            System.arraycopy(
+                    temp,
+                    0,
+                    treeNode.getData(),
+                    OFFSET_INTERNAL_NODE_KEY_BEGIN + ((indexToFill + 1) * (Long.BYTES + Pointer.BYTES)),
+                    Long.BYTES
+            );
+
+        }
+
+        return indexToFill;
     }
 }
