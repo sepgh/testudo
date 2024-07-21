1cf206048d4e6fc037a97aa2e9b9cde49637a34f
sepgh <sepehr.null+gpg@gmail.com>
2024-07-21 15:47:31 +0330

    feat: initialize scheme

---
+++ src/main/java/com/github/sepgh/testudo/scheme/Scheme.java
@@ -0,0 +1,84 @@
+package com.github.sepgh.testudo.scheme;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Data
+public class Scheme {
+    private String dbName;
+    private int version;
+    private List<Collection> collections;
+
+    @Builder
+    @Data
+    public static class Collection {
+        private int id;
+        private String name;
+        private List<Field> fields = new ArrayList<>();
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Collection that = (Collection) o;
+            return getId() == that.getId() && Objects.equals(getName(), that.getName()) && Objects.equals(getFields(), that.getFields());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName(), getFields());
+        }
+    }
+
+    @Data
+    @Builder
+    public static class Meta {
+        private String comment;
+        private String maxSize;
+        private int max;
+        private int min;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Meta meta = (Meta) o;
+            return getMax() == meta.getMax() && getMin() == meta.getMin() && Objects.equals(getComment(), meta.getComment()) && Objects.equals(getMaxSize(), meta.getMaxSize());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getComment(), getMaxSize(), getMax(), getMin());
+        }
+    }
+
+    @Data
+    @Builder
+    public static class Field {
+        private int id;
+        private String name;
+        private String type;
+        private Meta meta;
+        private boolean primary;
+        private boolean index;
+        private boolean indexUnique;
+        private String defaultValue;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Field field = (Field) o;
+            return getId() == field.getId() && isPrimary() == field.isPrimary() && isIndex() == field.isIndex() && isIndexUnique() == field.isIndexUnique() && Objects.equals(getName(), field.getName()) && Objects.equals(getType(), field.getType()) && Objects.equals(getMeta(), field.getMeta());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getName(), getType(), getMeta(), isPrimary(), isIndex(), isIndexUnique());
+        }
+    }
+}
---
+++ src/main/java/com/github/sepgh/testudo/scheme/SchemeComparator.java
@@ -0,0 +1,112 @@
+package com.github.sepgh.testudo.scheme;
+
+import lombok.AllArgsConstructor;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class SchemeComparator {
+    private final Scheme oldScheme;
+    private final Scheme newScheme;
+
+    // Todo: compare db name
+    public boolean Compare(SchemeComparisonListener comparisonListener) {
+
+        // Cases where there are no changes
+        if (oldScheme == newScheme || oldScheme.getVersion() == newScheme.getVersion()) {
+            return false;
+        }
+
+        if (oldScheme.getCollections().size() == newScheme.getCollections().size()){
+            boolean different = false;
+            for (int i = 0; i < oldScheme.getCollections().size(); i++) {
+                if (!oldScheme.getCollections().get(i).equals(newScheme.getCollections().get(i))) {
+                    different = true;
+                    break;
+                }
+            }
+            if (!different) {
+                return false;
+            }
+        }
+
+
+        // Comparing each collection fields if still exists otherwise removing collection
+        for (int i = 0; i < oldScheme.getCollections().size(); i++) {
+            for (Scheme.Collection oldSchemeCollection : oldScheme.getCollections()) {
+                Optional<Scheme.Collection> optionalCollection = newScheme.getCollections().stream().filter(collection -> collection.getId() == oldSchemeCollection.getId()).findFirst();
+
+                if (optionalCollection.isEmpty()) {
+                    comparisonListener.onChange(
+                            DifferenceReason.COLLECTION_REMOVED,
+                            oldSchemeCollection,
+                            null,
+                            null
+                    );
+                    continue;
+                }
+
+                Scheme.Collection newCollection = optionalCollection.get();
+                if (oldScheme.equals(newScheme))
+                    continue;
+
+                List<Scheme.Field> newCollectionUnComparedFields = new ArrayList<>(newCollection.getFields());
+
+                oldSchemeCollection.getFields().forEach(field -> {
+                    newCollectionUnComparedFields.remove(field);
+
+                    Optional<Scheme.Field> optionalCollectionField = newCollection.getFields().stream().filter(field1 -> field1.getId() == field.getId()).findFirst();
+                    if (optionalCollectionField.isEmpty()) {
+                        comparisonListener.onChange(
+                                DifferenceReason.FIELD_REMOVED,
+                                oldSchemeCollection,
+                                field,
+                                null
+                        );
+                        return;
+                    }
+
+                    Scheme.Field newCollectionField = optionalCollectionField.get();
+                    if (newCollectionField.isIndex() != field.isIndex() || newCollectionField.isIndexUnique() != field.isIndexUnique()){
+                        comparisonListener.onChange(
+                                DifferenceReason.FIELD_INDEX_CHANGED,
+                                oldSchemeCollection,
+                                field,
+                                newCollectionField
+                        );
+                    }
+
+                    if (!newCollectionField.getMeta().equals(field.getMeta())){
+                        comparisonListener.onChange(
+                                DifferenceReason.FIELD_META_CHANGED,
+                                oldSchemeCollection,
+                                field,
+                                newCollectionField
+                        );
+                    }
+
+                });
+
+                newCollectionUnComparedFields.forEach(field -> {
+                    comparisonListener.onChange(
+                            DifferenceReason.NEW_FIELD, newCollection, null, field
+                    );
+                });
+            }
+        }
+
+        return true;
+    }
+
+    public interface SchemeComparisonListener {
+        void onChange(DifferenceReason differenceReason, Scheme.Collection collection, @Nullable Scheme.Field oldField, @Nullable Scheme.Field newField);
+    }
+
+    public enum DifferenceReason {
+        NEW_FIELD, FIELD_META_CHANGED, FIELD_TYPE_CHANGED, FIELD_INDEX_CHANGED, FIELD_REMOVED, NEW_COLLECTION, COLLECTION_REMOVED
+    }
+
+}
